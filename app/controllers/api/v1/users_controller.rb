6d87326f75fa22e7aa596a275b85a692e3d0e30e
osigie <kenosagie88@gmail.com>
2022-12-21 16:50:33 +0100

    added update user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,17 @@
 class Api::V1::UsersController < ApplicationController
+
+  before_action :set_user, only: %i[show update]
+
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+
   # GET /users/1
   def show
     render json: User.find(params[:id])
@@ -18,5 +31,9 @@ class Api::V1::UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
