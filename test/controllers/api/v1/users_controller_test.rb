3fbbcc002a43afef9f58ba4088ecab442156e3df
osigie <kenosagie88@gmail.com>
2022-12-21 15:24:23 +0100

    initail commit

---
+++ test/controllers/api/v1/users_controller_test.rb
@@ -0,0 +1,19 @@
+require "test_helper"
+
+class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  setup do
+    @user = users(:one)
+  end
+
+  test "should show user" do
+    get api_vi_user_url(@user), as: :json
+    assert_response :success
+    # test to ensure response contain the correct email
+    json_response = JSON.parse(self.response.body)
+    assert_equal(@user.email, json_response[:email], "email is invalid")
+  end
+end
