b049622d452e09a322c387284747c80f9bece853
osigie <kenosagie88@gmail.com>
2022-12-21 15:50:27 +0100

    fixed the show user controller test

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -10,10 +10,10 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should show user" do
-    get api_vi_user_url(@user), as: :json
+    get api_v1_user_url(@user), as: :json
     assert_response :success
     # test to ensure response contain the correct email
     json_response = JSON.parse(self.response.body)
-    assert_equal(@user.email, json_response[:email], "email is invalid")
+    assert_equal(@user.email, json_response["email"], "email is invalid")
   end
 end
