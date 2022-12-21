0ddb03c89295dcb999f0f8f25df4117061c1a951
osigie <kenosagie88@gmail.com>
2022-12-21 16:36:06 +0100

    added create user and test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace  :api, defaults: {format: :json } do
     namespace :v1 do
-      resources :users, only: [:show]
+      resources :users, only: %i[show create]
       end
     end
   # Defines the root path route ("/")
