ba8aa5e2984445b6f7704e398364ed8cdede89fd
osigie <kenosagie88@gmail.com>
2022-12-22 12:17:15 +0100

    added creation of product and test

--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -20,4 +20,19 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
     get api_v1_products_url(), as: :json
     assert_response :success
   end
+
+  test " should create product" do
+    assert_difference("Product.count") do
+      post api_v1_products_url, params:{ product:{title:@product.title, price:@product.price, published:@product.published }}, headers:{Authorization: JsonWebToken.encode(user_id:@product.user_id)}, as: :json
+    end
+    assert_response :created
+  end
+
+
+  test " should forbid create product" do
+    assert_no_difference("Product.count") do
+      post api_v1_products_url(params:{ product:{title:@product.title, price:@product.price, published:@product.published, }}), as: :json
+    end
+    assert_response :forbidden
+  end
 end
