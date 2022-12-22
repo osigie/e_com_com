dcb5846b467bfe7a21b3cc1d80e7a76db6423deb
osigie <kenosagie88@gmail.com>
2022-12-22 10:11:56 +0100

    protected update and delete user endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::UsersController < ApplicationController
 
   before_action :set_user, only: %i[show update destroy]
+  before_action :check_owner, only: %i[update destroy]
 
 
   # DELETE /users/1
@@ -45,5 +46,10 @@ class Api::V1::UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  private
+  def check_owner
+    head :forbidden unless @user.id == current_user&.id
   end
 end
