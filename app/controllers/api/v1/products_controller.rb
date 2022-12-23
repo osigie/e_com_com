6a24a1b603fa93e51a17d6cc6b2f250f758a4bd3
osigie <kenosagie88@gmail.com>
2022-12-23 11:52:37 +0100

    created destroy and added test

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :set_product, only: %i[show, update]
+  before_action :set_product, only: %i[show update destroy]
   before_action :check_login, only: %i[create]
-  before_action :check_owner, only: %i[update]
+  before_action :check_owner, only: %i[update destroy]
   def show
     render json: Product.find(params[:id])
   end
@@ -12,7 +12,7 @@ class Api::V1::ProductsController < ApplicationController
 
   def create
     product = current_user.products.build(product_params)
-    if(product.save)
+    if product.save
       render json: product, status: :created
     else
       render json: product.errors, status: :unprocessable_entity
@@ -20,27 +20,29 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def update
-
-    if(@product.update(product_params))
+    if @product.update(product_params)
       render json: @product
     else
       render json: product.errors, status: :unprocessable_entity
     end
   end
 
-  private
-  def product_params
-    params.require(:product).permit(:title, :price, :published )
+  def destroy
+    @product.destroy
+    head 204
   end
 
+  private
 
+  def product_params
+    params.require(:product).permit(:title, :price, :published)
+  end
 
   def set_product
     @product = Product.find(params[:id])
   end
 
-  private
   def check_owner
-    head :forbidden  unless @product.user_id == current_user&.id
+    head :forbidden unless @product.user_id == current_user&.id
   end
 end
