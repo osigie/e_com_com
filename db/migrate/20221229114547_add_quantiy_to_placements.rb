50627a252fcd3213b65e91c07dc0d324a10de1ec
osigie <kenosagie88@gmail.com>
2022-12-29 14:40:40 +0100

    added increment and decrement of product quantities and calculated order total

---
+++ db/migrate/20221229114547_add_quantiy_to_placements.rb
@@ -0,0 +1,5 @@
+class AddQuantiyToPlacements < ActiveRecord::Migration[7.0]
+  def change
+    add_column :placements, :quantity, :integer, default: 0
+  end
+end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -4,11 +4,49 @@ class OrderTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test "should have positive total" do
-    order = orders(:one)
-    order.total = -1
-    assert_not order.valid?
+  setup do
+    @product1 = products(:one)
+    @product2 = products(:two)
+    @order = orders(:one)
   end
 
+  # test "should have positive total" do
+  #   order = orders(:one)
+  #   order.total = -1
+  #   assert_not order.valid?
+  # end
+
+
+  test "Should set total" do
+    @order.placements = [
+      Placement.new(product_id: @product1.id, quantity: 2),
+      Placement.new(product_id: @product2.id, quantity: 2)
+    ]
+    @order.set_total!
+    expected_total = (@product1.price * 2) + (@product2.price * 2)
+    assert_equal expected_total, @order.total
+  end
+
+
+
+
+
+
+  test 'builds 2 placements for the order' do
+    @order.build_placements_with_product_ids_and_quantities [
+                                                              { product_id: @product1.id, quantity: 2 },
+                                                              { product_id: @product2.id, quantity: 3 },
+                                                            ]
+
+    assert_difference('Placement.count', 2) do
+      @order.save
+    end
+  end
+
+
+  test "an order should not claim too much product then available" do
+    @order.placements << Placement.new(product_id:@product1.id,quantity:(1+@product1.quantity))
+    assert_not @order.valid?
+  end
 
 end
